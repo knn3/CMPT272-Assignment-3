4a152e5000099be527193c3f839b48a50f34d616
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-10-31 11:13:27 -0700

    Created Pigs supper class and subclasses

---
+++ Grey.ts
@@ -0,0 +1,7 @@
+class Grey extends Pigs {
+    swimming: string
+    constructor(n: string, b: string, h: number, w: number, s: string) {
+        super(n, b, h, w);
+        this.swimming = s;
+    }
+}
---
+++ Pigs.ts
@@ -0,0 +1,12 @@
+abstract class Pigs {
+    name: string;
+    breed: string;
+    height: number;
+    weight: number;
+    constructor(name: string, breed: string, height: number, weight: number) {
+        this.name = name;
+        this.breed = breed;
+        this.height = height;
+        this.weight = weight;
+    }
+}
