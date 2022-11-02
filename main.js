96ada3d5064d56ff475b955b20653ff7882d64be
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-11-01 17:08:31 -0700

    Added input form to main page

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-System.register(["./PigController", "./Grey"], function (exports_1, context_1) {
+System.register(["./PigController", "./Grey", "./White"], function (exports_1, context_1) {
     "use strict";
-    var PigController_1, Grey_1, pigs;
+    var PigController_1, Grey_1, White_1, pigs, select;
     var __moduleName = context_1 && context_1.id;
     // list of elements in local storage => for each element, display and delete button to that specific elenment
     function display() {
         var arr = pigs.getAll();
-        var dataTable = "";
+        var dataTable = "<tr><td>Name</td><td>Category</td><td></td><td></td></tr>";
         for (let i = 0; i < arr.length; i++) {
             dataTable += '<tr>';
             dataTable += '<td>' + arr[i].name;
-            dataTable += '<td>' + arr[i].breed;
-            dataTable += '<td>';
-            dataTable += '<td>';
+            dataTable += '<td>' + arr[i].category;
+            dataTable += '<td>' + '<button>More Info</button>';
+            dataTable += '<td>' + '<button>Delete</button>';
             dataTable += '<tr>';
         }
+        document.getElementById('display').innerHTML = dataTable;
     }
     return {
         setters: [
@@ -22,19 +23,54 @@ System.register(["./PigController", "./Grey"], function (exports_1, context_1) {
             },
             function (Grey_1_1) {
                 Grey_1 = Grey_1_1;
+            },
+            function (White_1_1) {
+                White_1 = White_1_1;
             }
         ],
         execute: function () {
             pigs = new PigController_1.PigController();
+            select = document.getElementById('Category');
+            //select category will dynamically display another attribute of that pigs
+            select.addEventListener('change', function (e) {
+                var html = "";
+                var index = select.selectedIndex;
+                var opt = select.options[index];
+                var value = opt.value;
+                if (value == 'Grey') {
+                    html += "<td>Swimming</td><td><input type='number' id='swiming'></td>";
+                }
+                else if (value == 'Chestnut') {
+                    html += "<td>Language</td><td><input type='number' id='language'></td>";
+                }
+                else if (value == 'White') {
+                    html += "<td>Running</td><td><input type='number' id='running'></td>";
+                    html += `
+        <select id="Category">
+            <option value="" disabled selected hidden>Choose one</option>
+            <option value="Yorkshire">Yorkshire</option>
+            <option value="Chester">Chester</option>
+            <option value="Lop">Lop</option>
+            <option value="Landrace">Landrace</option>
+        </select>  
+        `;
+                }
+                else if (value == 'Black') {
+                    html += "<td>Strength</td><td><input type='number' id='strength'></td>";
+                }
+                document.getElementById('attribute').innerHTML = html;
+            });
             document.getElementById('create').addEventListener('click', function () {
-                var pig = new Grey_1.Grey('Newt', 'Yorkshire', 10, 50, 65);
+                var pig = new White_1.White('Newt', 'Yorkshire', 10, 50, 65);
                 pigs.add(pig);
+                display();
             });
             document.getElementById('delete').addEventListener(('click'), function (e) {
                 var pig = new Grey_1.Grey('Newt', 'Yorkshire', 10, 50, 65);
                 console.log(pig.id);
                 pigs.delete(pig);
             });
+            display();
         }
     };
 });
--- main.ts
+++ main.ts
@@ -1,11 +1,50 @@
 import { PigController } from "./PigController";
 import { Grey } from "./Grey";
+import { White } from "./White";
 
 var pigs = new PigController();
+var select = document.getElementById('Category')! as HTMLSelectElement;
+//select category will dynamically display another attribute of that pigs
+
+select.addEventListener('change', function (e) {
+    var html = "";
+    var index = select.selectedIndex;
+    var opt = select.options[index];
+    var value = opt.value;
+
+    if (value == 'Grey') {
+        html += "<td>Swimming</td><td><input type='number' id='swiming'></td>"
+        
+    }
+    else if (value == 'Chestnut') {
+        html += "<td>Language</td><td><input type='number' id='language'></td>"
+    }
+    else if (value == 'White') {
+        html += "<td>Running</td><td><input type='number' id='running'></td>"
+        html += `
+        <tr>
+        <select id="Category">
+            <option value="" disabled selected hidden>Choose one</option>
+            <option value="Yorkshire">Yorkshire</option>
+            <option value="Chester">Chester</option>
+            <option value="Lop">Lop</option>
+            <option value="Landrace">Landrace</option>
+        </select>  
+        </tr>
+        `;
+    }
+    else if (value == 'Black') {
+        html += "<td>Strength</td><td><input type='number' id='strength'></td>"
+    }
     
+    document.getElementById('attribute')!.innerHTML = html;
+        
+})
+
 document.getElementById('create')!.addEventListener('click', function () {
-    var pig = new Grey('Newt', 'Yorkshire', 10, 50, 65);
+    var pig = new White('Newt', 'Yorkshire', 10, 50, 65);
     pigs.add(pig)
+    display();
 })
 
 document.getElementById('delete')!.addEventListener(('click'), function (e) {
@@ -20,16 +59,19 @@ document.getElementById('delete')!.addEventListener(('click'), function (e) {
 function display() {
     var arr = pigs.getAll();
 
-    var dataTable = "";
+    var dataTable = "<tr><td>Name</td><td>Category</td><td></td><td></td></tr>";
 
     for (let i = 0; i < arr.length; i++){
         dataTable += '<tr>';
         dataTable += '<td>'+arr[i].name;
-        dataTable += '<td>'+arr[i].breed;
-        dataTable += '<td>';
-        dataTable += '<td>';
+        dataTable += '<td>'+arr[i].category;
+        dataTable += '<td>'+'<button>More Info</button>';
+        dataTable += '<td>'+'<button>Delete</button>';
         dataTable += '<tr>';
 
     }
-    
+    document.getElementById('display')!.innerHTML = dataTable;
+
 }
+
+display()
