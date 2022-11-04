96d23dd1f4af5ce68bc3a4c36e2e34e0da3654e4
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-11-04 11:31:06 -0700

    Updated add pigs function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-System.register(["./PigController", "./Grey", "./White"], function (exports_1, context_1) {
+System.register(["./PigController", "./Grey", "./Chestnut", "./White", "./Black"], function (exports_1, context_1) {
     "use strict";
-    var PigController_1, Grey_1, White_1, pigs, select;
+    var PigController_1, Grey_1, Chestnut_1, White_1, Black_1, pigs, select;
     var __moduleName = context_1 && context_1.id;
     // list of elements in local storage => for each element, display and delete button to that specific elenment
     function display() {
@@ -24,8 +24,14 @@ System.register(["./PigController", "./Grey", "./White"], function (exports_1, c
             function (Grey_1_1) {
                 Grey_1 = Grey_1_1;
             },
+            function (Chestnut_1_1) {
+                Chestnut_1 = Chestnut_1_1;
+            },
             function (White_1_1) {
                 White_1 = White_1_1;
+            },
+            function (Black_1_1) {
+                Black_1 = Black_1_1;
             }
         ],
         execute: function () {
@@ -39,31 +45,31 @@ System.register(["./PigController", "./Grey", "./White"], function (exports_1, c
                 var opt = select.options[index];
                 var value = opt.value;
                 if (value == 'Grey') {
-                    html += "<td>Swimming</td><td><input type='number' id='swiming'></td>";
+                    html += "<td>Swimming</td><td><input type='number' id='swimming'></td>";
                     html2 += `
             <td>Breed</td>
             <td>
-                <select id="Breed">
+                <select id="Breed1">
                     <option value="" disabled selected>Choose one</option>
-                    <option value="Yorkshire">Yorkshire</option>
-                    <option value="Chester">Chester</option>
-                    <option value="Lop">Lop</option>
-                    <option value="Landrace">Landrace</option>
+                    <option value="York">York</option>
+                    <option value="Oink">Oink</option>
+                    <option value="Lance">Lance</option>
+                    <option value="Landrop">Landrop</option>
                 </select>  
             </td>
         `;
                 }
                 else if (value == 'Chestnut') {
-                    html += "<td>Language</td><td><input type='number' id='language'></td>";
+                    html += "<td>Language</td><td><input type='text' id='language'></td>";
                     html2 += `
             <td>Breed</td>
             <td>
-                <select id="Breed">
+                <select id="Breed2">
                     <option value="" disabled selected>Choose one</option>
-                    <option value="Yorkshire">Yorkshire</option>
-                    <option value="Chester">Chester</option>
-                    <option value="Lop">Lop</option>
-                    <option value="Landrace">Landrace</option>
+                    <option value="Asian">Asian</option>
+                    <option value="African">African</option>
+                    <option value="American">American</option>
+                    <option value="European">European</option>
                 </select>  
             </td>
         `;
@@ -73,7 +79,7 @@ System.register(["./PigController", "./Grey", "./White"], function (exports_1, c
                     html2 += `
             <td>Breed</td>
             <td>
-                <select id="Breed">
+                <select id="Breed3">
                     <option value="" disabled selected>Choose one</option>
                     <option value="Yorkshire">Yorkshire</option>
                     <option value="Chester">Chester</option>
@@ -88,12 +94,12 @@ System.register(["./PigController", "./Grey", "./White"], function (exports_1, c
                     html2 += `
             <td>Breed</td>
             <td>
-                <select id="Breed">
+                <select id="Breed4">
                     <option value="" disabled selected>Choose one</option>
-                    <option value="Yorkshire">Yorkshire</option>
-                    <option value="Chester">Chester</option>
-                    <option value="Lop">Lop</option>
-                    <option value="Landrace">Landrace</option>
+                    <option value="Yorkstrength">Yorkstrength</option>
+                    <option value="Chest">Chest</option>
+                    <option value="Loxing">Loxing</option>
+                    <option value="Lon Xe">Lon Xe</option>
                 </select>  
             </td>
         `;
@@ -102,8 +108,44 @@ System.register(["./PigController", "./Grey", "./White"], function (exports_1, c
                 document.getElementById('breedSelect').innerHTML = html2;
             });
             document.getElementById('create').addEventListener('click', function () {
-                var pig = new White_1.White('Newt', 'Yorkshire', 10, 50, 65);
-                pigs.add(pig);
+                var name = document.getElementById('Name');
+                var height = document.getElementById('Height');
+                var weight = document.getElementById('Weight');
+                var category = document.getElementById('Category');
+                var cate = category.options[category.selectedIndex].value;
+                var ability = document.getElementById('attribute');
+                if (cate == 'Grey') {
+                    var swimming = document.getElementById('swimming');
+                    var breed1 = document.getElementById('Breed1');
+                    var breedVal1 = breed1.options[breed1.selectedIndex].value;
+                    var pig1 = new Grey_1.Grey(name.value, breedVal1, parseInt(height.value), parseInt(weight.value), parseInt(swimming.value));
+                    pigs.add(pig1);
+                    console.log(pig1);
+                }
+                else if (cate == 'Chestnut') {
+                    var language = document.getElementById('language');
+                    var breed2 = document.getElementById('Breed2');
+                    var breedVal2 = breed2.options[breed2.selectedIndex].value;
+                    var pig2 = new Chestnut_1.Chestnut(name.value, breedVal2, parseInt(height.value), parseInt(weight.value), language.value);
+                    pigs.add(pig2);
+                    console.log(pig2);
+                }
+                else if (cate == 'White') {
+                    var running = document.getElementById('running');
+                    var breed3 = document.getElementById('Breed3');
+                    var breedVal3 = breed3.options[breed3.selectedIndex].value;
+                    var pig3 = new White_1.White(name.value, breedVal3, parseInt(height.value), parseInt(weight.value), parseInt(running.value));
+                    pigs.add(pig3);
+                    console.log(pig3);
+                }
+                else if (cate == 'Black') {
+                    var strength = document.getElementById('strength');
+                    var breed4 = document.getElementById('Breed4');
+                    var breedVal4 = breed4.options[breed4.selectedIndex].value;
+                    var pig4 = new Black_1.Black(name.value, breedVal4, parseInt(height.value), parseInt(weight.value), parseInt(strength.value));
+                    pigs.add(pig4);
+                    console.log(pig4);
+                }
                 display();
             });
             document.getElementById('delete').addEventListener(('click'), function (e) {
--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import { PigController } from "./PigController";
 import { Grey } from "./Grey";
+import { Chestnut } from "./Chestnut";
 import { White } from "./White";
+import { Black } from "./Black";
 
 var pigs = new PigController();
 var select = document.getElementById('Category')! as HTMLSelectElement;
@@ -15,32 +17,32 @@ select.addEventListener('change', function (e) {
     var value = opt.value;
 
     if (value == 'Grey') {
-        html += "<td>Swimming</td><td><input type='number' id='swiming'></td>"
+        html += "<td>Swimming</td><td><input type='number' id='swimming'></td>"
         html2 += `
             <td>Breed</td>
             <td>
-                <select id="Breed">
+                <select id="Breed1">
                     <option value="" disabled selected>Choose one</option>
-                    <option value="Yorkshire">Yorkshire</option>
-                    <option value="Chester">Chester</option>
-                    <option value="Lop">Lop</option>
-                    <option value="Landrace">Landrace</option>
+                    <option value="York">York</option>
+                    <option value="Oink">Oink</option>
+                    <option value="Lance">Lance</option>
+                    <option value="Landrop">Landrop</option>
                 </select>  
             </td>
         `;
         
     }
     else if (value == 'Chestnut') {
-        html += "<td>Language</td><td><input type='number' id='language'></td>"
+        html += "<td>Language</td><td><input type='text' id='language'></td>"
         html2 += `
             <td>Breed</td>
             <td>
-                <select id="Breed">
+                <select id="Breed2">
                     <option value="" disabled selected>Choose one</option>
-                    <option value="Yorkshire">Yorkshire</option>
-                    <option value="Chester">Chester</option>
-                    <option value="Lop">Lop</option>
-                    <option value="Landrace">Landrace</option>
+                    <option value="Asian">Asian</option>
+                    <option value="African">African</option>
+                    <option value="American">American</option>
+                    <option value="European">European</option>
                 </select>  
             </td>
         `;
@@ -50,7 +52,7 @@ select.addEventListener('change', function (e) {
         html2 += `
             <td>Breed</td>
             <td>
-                <select id="Breed">
+                <select id="Breed3">
                     <option value="" disabled selected>Choose one</option>
                     <option value="Yorkshire">Yorkshire</option>
                     <option value="Chester">Chester</option>
@@ -65,12 +67,12 @@ select.addEventListener('change', function (e) {
         html2 += `
             <td>Breed</td>
             <td>
-                <select id="Breed">
+                <select id="Breed4">
                     <option value="" disabled selected>Choose one</option>
-                    <option value="Yorkshire">Yorkshire</option>
-                    <option value="Chester">Chester</option>
-                    <option value="Lop">Lop</option>
-                    <option value="Landrace">Landrace</option>
+                    <option value="Yorkstrength">Yorkstrength</option>
+                    <option value="Chest">Chest</option>
+                    <option value="Loxing">Loxing</option>
+                    <option value="Lon Xe">Lon Xe</option>
                 </select>  
             </td>
         `;
@@ -83,8 +85,51 @@ select.addEventListener('change', function (e) {
 })
 
 document.getElementById('create')!.addEventListener('click', function () {
-    var pig = new White('Newt', 'Yorkshire', 10, 50, 65);
-    pigs.add(pig)
+    var name = document.getElementById('Name')! as HTMLInputElement;
+    var height = document.getElementById('Height')! as HTMLInputElement;
+    var weight = document.getElementById('Weight')! as HTMLInputElement;
+    var category = document.getElementById('Category')! as HTMLSelectElement;
+    var cate = category.options[category.selectedIndex].value;
+    var ability = document.getElementById('attribute')! as HTMLInputElement;
+
+    if (cate == 'Grey') {
+        var swimming = document.getElementById('swimming')! as HTMLInputElement;
+        var breed1 = document.getElementById('Breed1')! as HTMLSelectElement;
+        var breedVal1 = breed1.options[breed1.selectedIndex].value;
+        var pig1 = new Grey(name.value, breedVal1, parseInt(height.value), parseInt(weight.value), parseInt(swimming.value));
+        pigs.add(pig1)
+        console.log(pig1)
+
+    }
+    else if (cate == 'Chestnut') {
+        var language = document.getElementById('language')! as HTMLInputElement;
+        var breed2 = document.getElementById('Breed2')! as HTMLSelectElement;
+        var breedVal2 = breed2.options[breed2.selectedIndex].value;
+        var pig2 = new Chestnut(name.value, breedVal2, parseInt(height.value), parseInt(weight.value), language.value);
+        pigs.add(pig2)
+        console.log(pig2)
+        
+    }
+    else if (cate == 'White') {
+        var running = document.getElementById('running')! as HTMLInputElement;
+        var breed3 = document.getElementById('Breed3')! as HTMLSelectElement;
+        var breedVal3 = breed3.options[breed3.selectedIndex].value;
+        var pig3 = new White(name.value, breedVal3, parseInt(height.value), parseInt(weight.value), parseInt(running.value));
+        pigs.add(pig3)
+        console.log(pig3)
+
+        
+    }
+    else if (cate == 'Black') {
+        var strength = document.getElementById('strength')! as HTMLInputElement;
+        var breed4 = document.getElementById('Breed4')! as HTMLSelectElement;
+        var breedVal4 = breed4.options[breed4.selectedIndex].value;
+        var pig4 = new Black(name.value, breedVal4, parseInt(height.value), parseInt(weight.value), parseInt(strength.value));
+        pigs.add(pig4)
+        console.log(pig4)
+
+    }
+
     display();
 })
 
